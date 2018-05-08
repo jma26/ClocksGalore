2a4d89f21bd6f6e010f5c78c182115711df23e8d
jma26 <j.masonoma17@gmail.com>
2018-05-07 23:48:28 -0700

    Add setHawaii function for hawaii time

--- main.js
+++ main.js
@@ -78,10 +78,30 @@ function setCentral() {
     minuteHand.style.transform = `rotate(${central_minuteDegrees}deg)`;
 }
 
+function setHawaii() {
+    secondHand = document.querySelectorAll(".second-hand")[4];
+    hourHand = document.querySelectorAll(".hour-hand")[4];
+    minuteHand = document.querySelectorAll(".min-hand")[4];
+
+    var hawaii = moment();
+    var hawaii_seconds = hawaii.second();
+    var hawaii_hour = hawaii.hour() - 3;
+    var hawaii_minutes = hawaii.minute();
+
+    var hawaii_secondsDegrees = ((hawaii_seconds/60) * 360) + 90;
+    var hawaii_hourDegrees = ((hawaii_hour/12) * 360) + 90;
+    var hawaii_minuteDegrees = ((hawaii_minutes/60) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${hawaii_secondsDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hawaii_hourDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${hawaii_minuteDegrees}deg)`;
+}
+
 
   setInterval(function() {
       setPacific();
       setEastern();
       setMountain();
       setCentral();
+      setHawaii();
   }, 1000);
